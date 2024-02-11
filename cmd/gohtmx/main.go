ab7b58283b5ad74a73b74aab0a936c24089f0755
Tony Gilkerson <tgilkerson@miletwo.us>
2024-02-11 10:38:43 -0500

    a little refactor

--- cmd/gohtmx/main.go
+++ cmd/gohtmx/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/tonygilkerson/gohtmx/internal/gohtmx"
 )
@@ -14,41 +13,31 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	//
-	// Get environment Variables
-	//
-	webRoot, exists := os.LookupEnv("WEB_ROOT")
-	if exists {
-		log.Printf("Using environment variable WEB_ROOT: %v", webRoot)
-	} else {
-		webRoot, _ = os.Executable() 
-		log.Printf("WEB_ROOT environment variable not set, using default value: %v", webRoot)
-	}
-
-	//
 	// Create Server
 	//
 	mux := http.NewServeMux()
-  svr := &http.Server{Addr: ":8080", Handler: mux,}
+	svr := &http.Server{Addr: ":8080", Handler: mux}
 
-	// Create a new site instance
-	site := gothmx.NewSite(webRoot)
+	//
+	// Init handler context
+	//
+	hctx := gohtmx.NewHandlerContext()
 
 	//
 	// Define routes
 	//
-	mux.HandleFunc("/", site.HomeHandler)
-	
-	mux.HandleFunc("/hi", gothmx.HiHandler)
-	mux.HandleFunc("/greeting", gothmx.GreetingHandler)
+	mux.HandleFunc("/", hctx.HomeHandler)
+	mux.HandleFunc("/say-hi", hctx.SayHiHandler)
+	mux.HandleFunc("/say-hi-response", hctx.SayHiResponseHandler)
 
+	// For the live and readness probes
 	mux.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
-		_,_ = w.Write([]byte("ok"))
+		_, _ = w.Write([]byte("ok"))
+	})
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("ok"))
 	})
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		_,_ = w.Write([]byte("ok"))
-	})
-	
 	log.Printf("Listening on: %v", svr.Addr)
 	log.Fatal(svr.ListenAndServe())
 }
--- internal/gohtmx/gothmx.go
+++ internal/gohtmx/gothmx.go
@@ -1,78 +1,31 @@
-package gothmx
+package gohtmx
 
 import (
 	"log"
-	"net/http"
 	"os"
 	"text/template"
 )
 
-type Site struct {
-	Heading      string
-	Intro        string
-	HomeTemplate *template.Template
+type HandlerContext struct {
+	webRoot string
+	PageTemplates map[string] *template.Template
 }
 
-func NewSite(webRoot string) *Site {
+func NewHandlerContext() *HandlerContext {
 
-	// Create initial instance of site
-	site := Site{
-		Heading:      "Welcome, from Go HTMX!",
-		Intro:        "The home page for my Go with HTMX play site",
-		HomeTemplate: &template.Template{},
+	webRoot, exists := os.LookupEnv("WEB_ROOT")
+	if exists {
+		log.Printf("Using environment variable WEB_ROOT: %v", webRoot)
+	} else {
+		webRoot, _ = os.Executable()
+		log.Printf("WEB_ROOT environment variable not set, using default value: %v", webRoot)
 	}
 
-	// Configure Home Template
-	homeTmplFile := webRoot+"/www/home.gotmpl"
-	log.Printf("Load home template using: %v", homeTmplFile)
-	homeTmpl, err := os.ReadFile(homeTmplFile)
-	doOrDie(err)
-	
-	site.HomeTemplate, _ = template.New("HomePage").Parse(string(homeTmpl))
+	var hc HandlerContext
+	hc.webRoot = webRoot
+	hc.PageTemplates = make(map[string] *template.Template)
 
-	return &site
-}
-
-func (s *Site) HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html;charset=utf-8")
-	w.Header().Set("Cache-Control", "no-store")
-
-	err := s.HomeTemplate.Execute(w, s)
-	doOrDie(err)
-
-}
-
-func HiHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html;charset=utf-8")
-	w.Header().Set("Cache-Control", "no-store")
-
-	homePage := []byte(`
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<script src="https://unpkg.com/htmx.org@1.9.4/dist/htmx.min.js"></script>
-		</head>
-
-		<body>
-			<h1>Hi</h1>
-			<button hx-get="/greeting" hx-swap="#data-container">Show Greeting</button>
-			<div id="data-container"></div>
-		</body>
-		</html>
-	`)
-
-	_, _ = w.Write(homePage)
-
-}
-
-func GreetingHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
-	w.Header().Set("Cache-Control", "no-store")
-
-	homePage := []byte("Hello from the gohtmx project!")
-
-	_, _ = w.Write(homePage)
-
+	return &hc
 }
 
 func doOrDie(err error) {
